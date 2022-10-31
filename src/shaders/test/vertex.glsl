f2ffbcba76b7acdf9534f4b488393d43f0a8edab
Jacopo <jacopo.v@pm.me>
2022-10-31 21:53:14 +0000

    init

---
+++ src/shaders/test/vertex.glsl
@@ -0,0 +1,35 @@
+
+
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 modelMatrix;
+uniform float uTime;
+uniform vec3 uMouse;
+
+attribute vec3 position;
+
+
+
+void main()
+{
+
+    //***** https://learnopengl.com/Getting-started/Coordinate-Systems *****//
+
+    // 1)postion our geometry - coordinates your object begins in.
+    
+    vec4 localPosition = vec4(position , 1.0);
+    
+    // 2)transform the local coordinates to world-space coordinates
+    vec4 worldPosition = modelMatrix * localPosition;
+    
+    // 3)transform the world coordinates to view-space coordinates - seen from the camera/viewer point of view
+    vec4 viewPosition = viewMatrix * worldPosition;
+
+    // 4)project view coordinates to clip coordinates and transform to screen coordinates
+    vec4 clipPosition = projectionMatrix * viewPosition;
+
+    gl_Position = clipPosition;
+    gl_PointSize = 2.;
+
+
+}
